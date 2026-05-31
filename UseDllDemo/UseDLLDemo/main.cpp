860ca4b269874d4e08505c3ce6655e346e30c228
caomw2 <caomw@hfut.edu.cn>
2026-06-01 00:53:08 +0800

    Add files via upload

---
+++ UseDllDemo/UseDLLDemo/main.cpp
@@ -0,0 +1,23 @@
+
+#include <string>
+#include <iostream>
+
+#include <dlldemolib.h>
+
+int main(int argc, const char** argv)
+{
+
+
+	std::string name = "Mingwei Cao";
+	int age = 20;
+
+
+	disp(age, name);
+
+	std::cout << "The end." << std::endl;
+
+	return EXIT_SUCCESS;
+}
+
+
+
